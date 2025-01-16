8904d27278a62cfd9af70bca234b00970fbebed0
Kaustav Chakraborty <2004.k.c2@gmail.com>
2025-01-16 11:20:38 +0530

    Flask Code Updated

--- app.py
+++ app.py
@@ -19,8 +19,11 @@ model.eval()
 # Preprocessing transformation
 preprocess = transforms.Compose([
     transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(20),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # ImageNet normalization
 ])
 
 @app.route('/')
